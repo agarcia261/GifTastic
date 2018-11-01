4f0b744d4c36b57f59ef1e513e3ba1d8d4fba742
Angel <agarcia261@gmail.com>
2018-11-01 14:29:32 -0400

    final version

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,48 +1,99 @@
 $(document).ready(function() {
 
-    var queryURL="https://api.giphy.com/v1/gifs/search?api_key=qlZbX0Qs6D9ngOxm7MgasFYS41eu86s7&q=monkey&limit=25&offset=0&rating=G&lang=en"
-    //var apiKey="qlZbX0Qs6D9ngOxm7MgasFYS41eu86s7"
+    var animalList=["monkey", "cats", "dogs"]
+    var objresponse={}
+    addNewButton();
 
-    var apiReady=false;
+    function addNewButton(){
+        $(".buttons").empty()
 
+        for (var i= 0; i<animalList.length; i++){
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function(response) {
-        apiReady=true
-        console.log(response)
-        for (var i=0; i<response.data.length; i++){
-            var imagesDiv = $("<div>");
-            var rating=$("<p>");
-            rating.text("Rating: "+ response.data[i].rating)
-            imagesDiv.append(rating)
-            var images = $("<img>");
-            images.attr("src",response.data[i].images.fixed_height.url)
-            images.addClass("gif-img")
-            imagesDiv.append(images)
-            $(".gif-space").append(imagesDiv)
+            var button = $("<button>")
+            button.addClass("buttonsClass btn btn-default")
+            button.attr("animal",animalList[i])
+            button.text(animalList[i])
+            $(".buttons").append(button)
+        }
+    }
+
+    $(document).on("click", ".buttonsClass", clickFunction);
+
+    function clickFunction(event){
+        var animal=$(this).attr("animal")
+        var queryURL="https://api.giphy.com/v1/gifs/search?api_key=qlZbX0Qs6D9ngOxm7MgasFYS41eu86s7&q="+animal+"&limit=10&offset=0&rating=G&lang=en"
+        //var apiKey="qlZbX0Qs6D9ngOxm7MgasFYS41eu86s7"
+    
+        var apiReady=false;
+    
+    
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+          }).then(function(response) {
+
+            objresponse=response
+            apiReady=true
+
+            $(".gif-space").empty()
+
+            for (var i=0; i<response.data.length; i++){
+
+                var imagesDiv = $("<div>");
+                var rating=$("<p>");
+                rating.text("Rating: "+ response.data[i].rating)
+                imagesDiv.append(rating)
+
+                var images = $("<img>");
+                images.attr("src",response.data[i].images.fixed_height_still.url)
+                images.attr("index",i)
+                images.attr("status","still")
+                images.addClass("gif-img")
+                imagesDiv.append(images)
+                $(".gif-space").append(imagesDiv)
+    
+            }
+    
+        });
+    }
+
+    $(document).on("click", "#submit-button", addButton);
+
+    function addButton(event){
+        event.preventDefault()
+        if (animalList.indexOf($("#form-input").val().trim())==-1){
+        animalList.push($("#form-input").val().trim())
+        addNewButton();
+        }
+        else{
+            alert($("#form-input").val() + " already exist")
+        }
+        $("#form-input").val("")
+
+    }
+
+    $(document).on("click", ".gif-img", gifControl);
+
+    function gifControl(event){
+
+        var index=Number($(this).attr("index"))
+        if($(this).attr("status")=="still"){
+            $(this).attr("src",objresponse.data[index].images.fixed_height.url)
+            $(this).attr("status","animated")
+
+        }
+        else{
+            $(this).attr("src",objresponse.data[index].images.fixed_height_still.url)
+            $(this).attr("status","still")
+
 
         }
 
-    });
-
-    $(document).on("click", ".answerbtn", ansFunction);
-
-    function ansFunction(){
-        stopwatch.inBetQuestions=true
-        questionObj.verifyAnswer(this)
     }
 
-    $(document).on("click", ".restart-questions", restartGame);
 
-    function restartGame(){
-
-        questionObj.restartQuestions()
-    }
-
-   $(document).on("mouseover", ".button", mouseOverFunction);
-   $(document).on("mouseleave", ".button", mouseLeaveFunction);
+   $(document).on("mouseover", ".btn", mouseOverFunction);
+   $(document).on("mouseleave", ".btn", mouseLeaveFunction);
 
     function mouseOverFunction(){
         $(this).css("background-color", "gray");
